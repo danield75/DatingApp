d72563a1cfd60cf6f295e40a6049df1610ffbb94
Daniel Drion <daniel.drion@telenet.be>
2025-02-20 14:17:58 +0100

    Added UsersController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = context.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+}
