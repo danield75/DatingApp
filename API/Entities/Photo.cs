4cf95417cc899c0c4f5b39e9573f3f2d31168288
Daniel Drion <daniel.drion@telenet.be>
2025-04-09 14:19:54 +0200

    Entity Framework relationship AppUsers - Photos

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,5 +1,8 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace API.Entities;
+
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
@@ -9,4 +12,9 @@ public class Photo
     public bool IsMain { get; set; }
 
     public string? PublicId { get; set; }
+
+    // Navigation properties
+    public int AppUserId { get; set; }
+
+    public AppUser AppUser { get; set; } = null!;
 }
