1f2fb393a54fe378c03eb7278eadc87b7fdbbff9
Daniel Drion <daniel.drion@telenet.be>
2025-04-11 14:24:21 +0200

    Using explicit type variable

--- API/Program.cs
+++ API/Program.cs
@@ -19,18 +19,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
+using IServiceScope scope = app.Services.CreateScope();
+IServiceProvider services = scope.ServiceProvider;
 try
 {
-    var context = services.GetRequiredService<DataContext>();
+    DataContext context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
     await Seed.SeedUsers(context);
 }
 catch (Exception ex)
 {
 
-    var logger = services.GetRequiredService<ILogger<Program>>();
+    ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occurred during migration");
 }
 
