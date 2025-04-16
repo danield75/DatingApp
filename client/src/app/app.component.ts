189858858115f4afe4af4e042cb0b534a1b5f268
Daniel Drion <daniel.drion@telenet.be>
2025-04-16 15:32:38 +0200

    Removed unused HomeComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -21,7 +20,7 @@ export class AppComponent implements OnInit {
   setCurrentUser() {
     const userString = localStorage.getItem('user');
     if (!userString) return;
-    
+
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
