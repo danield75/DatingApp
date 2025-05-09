103fa730986cc0cf155ce62f711421fcbb6cc60c
Daniel Drion <daniel.drion@telenet.be>
2025-05-09 16:28:50 +0200

    Deleting photos in the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -48,5 +48,20 @@ export class MembersService {
       })
     );
   }
+
+  deletePhoto(photo: Photo) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+      tap(() => {
+        this.members.update(members => 
+          members.map(m => {
+            if(m.photos.includes(photo)) {
+              m.photos = m.photos.filter(p => p.id !== photo.id);
+            }            
+            return m;
+          })
+        );
+      })
+    );
+  }
 }
 
