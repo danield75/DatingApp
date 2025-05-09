ae195e3484b1445fd4d6bb7e4ad6be77c01e236b
Daniel Drion <daniel.drion@telenet.be>
2025-05-09 14:01:05 +0200

    Setting the main photo

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -87,4 +87,38 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
 
         return BadRequest("Problem adding photo");
     }
+
+    [HttpPut("set-main-photo/{photoId:int}")]
+    public async Task<ActionResult> SetMainPhotoAsync(int photoId)
+    {
+        AppUser? user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+        if (user == null)
+        {
+            return BadRequest("Could not find user");
+        }
+
+        Photo? photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+        if (photo == null)
+        {
+            return NotFound();
+        }
+        if (photo.IsMain)
+        {
+            return BadRequest("This is already your main photo");
+        }
+
+        Photo? currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+        if (currentMain != null)
+        {
+            currentMain.IsMain = false;
+        }
+
+        photo.IsMain = true;
+        if (await userRepository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Problem setting main photo");
+    }
 }
